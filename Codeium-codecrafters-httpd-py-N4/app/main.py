15e987c58de01d0f7a58c64a210607c83e0dc40d
Carl From InternetOfBugs <Carl@InternetOfBugs.com>
2024-09-01 23:33:44 -0500

    codeium files

--- Codeium-codecrafters-httpd-py-N4/app/main.py
+++ Codeium-codecrafters-httpd-py-N4/app/main.py
@@ -1,8 +1,9 @@
+import os
 import socket
 import threading
 
 
-def handle_request(conn, addr):
+def handle_request(conn, addr, files_dir):
     request = conn.recv(1024)
     if request.startswith(b"GET / "):
         conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
@@ -15,12 +16,25 @@ def handle_request(conn, addr):
             if header.startswith(b"User-Agent:"):
                 user_agent = header.split(b": ", 1)[1]
                 conn.sendall(b'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ' + bytes(str(len(user_agent)), 'utf-8') + b'\r\n\r\n' + user_agent)
+    elif b"GET /files/" in request:
+        filename = request.split()[1].split(b"/files/")[1]
+        if os.path.isfile(os.path.join(files_dir, filename)):
+            with open(os.path.join(files_dir, filename), 'rb') as f:
+                file_contents = f.read()
+            conn.sendall(b'HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ' + bytes(str(len(file_contents)), 'utf-8') + b'\r\n\r\n' + file_contents)
+        else:
+            conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
     else:
         conn.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
     conn.close()
 
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory")
+    args = parser.parse_args()
+
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
@@ -30,7 +44,7 @@ def main():
 
     while True:
         conn, addr = server_socket.accept()
-        t = threading.Thread(target=handle_request, args=(conn, addr))
+        t = threading.Thread(target=handle_request, args=(conn, addr, args.directory))
         t.start()
 
     server_socket.close() # close the server
