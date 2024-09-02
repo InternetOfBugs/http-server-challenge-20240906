f829b342413716cf081e9659a405de013316ad15
Carl From InternetOfBugs <Carl@InternetOfBugs.com>
2024-09-01 23:33:44 -0500

    jetbrains echo

--- JetBrains-codecrafters-httpd-py-N5/app/main.py
+++ JetBrains-codecrafters-httpd-py-N5/app/main.py
@@ -1,17 +1,18 @@
 import socket  # noqa: F401
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, _ = server_socket.accept()  # wait for client
-
     try:
         data = client_socket.recv(1024).decode("utf-8")
         request_line = data.split("\n")[0]
         url = request_line.split(" ")[1]
-        if url == "/":
+        if url.startswith("/echo/"):
+            response_body = url.split("/echo/")[1]
+            response_headers = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n"
+            client_socket.sendall(response_headers.encode() + response_body.encode())
+        elif url == "/":
             client_socket.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
         else:
             client_socket.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
@@ -19,9 +20,6 @@ def main():
         print(e)
     finally:
         client_socket.close()
-
-
-
 
 if __name__ == "__main__":
     main()
