3845b25d221be3b15f06e017e7a90ef0bd3b9678
Carl from InternetOfBugs <carl@internetofbugs.com>
2024-09-18 15:49:12 -0500

    O1 Initial 200_OK attempt

--- o1-codecrafters-http-server-python/app/main.py
+++ o1-codecrafters-http-server-python/app/main.py
@@ -1,15 +1,23 @@
-import socket  # noqa: F401
 
+import socket  # noqa: F401
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    while True:
+        client_connection, client_address = server_socket.accept()
+        with client_connection:
+            # Read the request (you can ignore the content)
+            request = client_connection.recv(1024)
+            # Prepare the HTTP response
+            response = b"HTTP/1.1 200 OK\r\n\r\n"
+            # Send the HTTP response
+            client_connection.sendall(response)
+            # Close the connection
+            client_connection.close()
 
 if __name__ == "__main__":
     main()
+
